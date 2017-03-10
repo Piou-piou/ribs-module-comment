0a85bca4eae7be900f9b40b7175f032d96ec8b58
Piou-piou <pilloud.anthony@gmail.com>
2017-03-10 19:11:39 +0100

    change table and create function to get comments

--- app/controller/Comment.php
+++ app/controller/Comment.php
@@ -2,19 +2,48 @@
 	namespace modules\comment\app\controller;
 	
 	
+	use core\App;
+	
 	class Comment {
-		
-		
-		
 		//-------------------------- BUILDER ----------------------------------------------------------------------------//
-		public function __construct() {
-			
-		}
 		//-------------------------- END BUILDER ----------------------------------------------------------------------------//
 		
 		
 		
 		//-------------------------- GETTER ----------------------------------------------------------------------------//
+		private function getRender($values) {
+			
+		}
+		
+		/**
+		 * @param $table
+		 * @param $nom_id_table
+		 * @param $id_in_table
+		 * function wich get all comments of an other module like a article of a blog
+		 * after all coments was getted it will call getRender to use twig to render them
+		 */
+		public function getComments($table, $nom_id_table, $id_in_table) {
+			$dbc = App::getDb();
+			
+			$query = $dbc->select()->from("_comment_all")->where("nom_table", "=", $table, "AND")
+				->where("nom_id_table", "=", $nom_id_table, "AND")->where("ID_in_table", "=", $id_in_table)->get();
+			
+			if (count($query) > 0) {
+				$values = [];
+				
+				foreach ($query as $obj) {
+					$values[] = [
+						"comment" => $obj->comment,
+						"date" => $obj->date,
+						"first_name" => $obj->first_name,
+						"last_name" => $obj->last_name,
+						"ID_identite" => $obj->ID_identite,
+					];
+				}
+				
+				$this->getRender($values);
+			}
+		}
 		//-------------------------- END GETTER ----------------------------------------------------------------------------//
 		
 		
--- install.php
+++ install.php
@@ -1,13 +1,14 @@
 <?php
 	$requete = "
 		DROP TABLE IF EXISTS _comment_all ;
-		CREATE TABLE _comment_all (ID_comment__comment_all INT AUTO_INCREMENT NOT NULL,
-		comment__comment_all TEXT,
-		date__comment_all DATETIME,
-		first_name__comment_all VARCHAR(50),
-		last_name__comment_all VARCHAR(50),
-		ID_identite__comment_all INT,
-		ID_module__comment_all INT,
-		ID_in_module__comment_all INT,
-		PRIMARY KEY (ID_comment__comment_all)) ENGINE=InnoDB;
+		CREATE TABLE _comment_all (ID_comment INT AUTO_INCREMENT NOT NULL,
+		comment TEXT,
+		date DATETIME,
+		first_name VARCHAR(50),
+		last_name VARCHAR(50),
+		ID_identite INT,
+		nom_table VARCHAR(255),
+		nom_id_table VARCHAR(255),
+		ID_in_table INT,
+		PRIMARY KEY (ID_comment)) ENGINE=InnoDB;
 	";
